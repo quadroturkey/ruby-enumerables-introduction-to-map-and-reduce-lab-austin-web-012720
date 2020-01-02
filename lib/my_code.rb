057e9b85f5a4e0603d9ce8cdb48fd1570182a8a3
Reese M Bowman <reesemb1@gmail.com>
2020-01-02 20:42:14 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,3 +1,10 @@
 def map_to_negativize(source_array)
+  result = []
+  index = 0 
+  while index < source_array.length do
+    new_num = source_array[index] *= -1
+    result << new_num
+    index += 1 
+  end
   
 end
