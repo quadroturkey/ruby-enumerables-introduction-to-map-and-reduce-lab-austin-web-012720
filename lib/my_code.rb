a7253f891514baabf7a896190edb8108eddd6a9d
Reese M Bowman <reesemb1@gmail.com>
2020-01-02 21:01:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -42,11 +42,8 @@ end
 # Reduce methods below
 
 def reduce_to_total(source_array, starting_point)
-  if starting_point
-    result = starting_point
-  else
-    result = 0 
-  end
+  result = 0
+  result = starting_point
   index = 0 
   while index < source_array.length do
     result += source_array[index]
